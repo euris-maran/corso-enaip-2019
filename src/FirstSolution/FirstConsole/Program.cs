664c358aec9175ec7f5694e30f28c65b33d48948
euris-maran <daniel.maran@euris.it>
2019-03-01 12:09:18 +0100

    Console operatori tipi nativi if

---
+++ src/FirstSolution/FirstConsole/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //single row comment
+
+            /*
+             * Multirow comment
+             */
+
+            int i;
+            short s;
+            uint ui;
+            long l;
+            float f;
+            double dd;
+            decimal d;
+            string st;
+            char c;
+            bool b;
+            byte bt;
+
+            i = 1;
+            i = i + 1;
+            //i = i++;
+            //i = ++i;
+            //i += 1;
+
+            if (i > 1 && i < 10 && i != 7)
+            {
+
+            }
+            else if (i >= 2 || i == 5)
+            {
+
+            }
+            else
+            {
+
+            }
+
+            
+
+            string str = "ciao";
+            str = string.Concat(str, " Mario");
+
+            Console.WriteLine(str);
+
+            Console.ReadLine();
+        }
+    }
+}
