10ad0e155a7f78cb16b666b46ad111b6744aeeec
euris-maran <daniel.maran@euris.it>
2019-03-04 11:24:14 +0100

    Iterators

---
+++ src/FirstSolution/FirstConsole/Iterations.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsole
+{
+    class Iterations
+    {
+        void Iterate()
+        {
+            int i = 0;
+            for (i = 0; i < 3; i++)
+            {
+                System.Diagnostics.Debug.WriteLine(i);
+            }
+
+            for (i = 3; i > 0; i--)
+            {
+                System.Diagnostics.Debug.WriteLine(i);
+            }
+
+            i = 0;
+            while (i < 3)
+            {
+                System.Diagnostics.Debug.WriteLine(i);
+                i++;
+            }
+
+            i = 0;
+            do
+            {
+                System.Diagnostics.Debug.WriteLine(i);
+                i++;
+            } while (i < 3);
+        }
+    }
+}
