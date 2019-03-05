d2888f19a92ccf774e999bb04645d4093ffe837e
euris-maran <daniel.maran@euris.it>
2019-03-05 10:05:03 +0100

    Scheletro esercizio parentesi

---
+++ src/Brackets/Brackets/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool ok = CheckBrackets(@"
+                namespace Brackets
+                {
+                    class Program
+                    {
+                        static void Main(string[] args)
+                        {
+                            bool ok = CheckBrackets("")
+
+                            Console.ReadLine();
+                        }
+                        static bool CheckBrackets(string text)
+                        {
+
+                        }
+                    }
+                }");
+
+            string strOk = ok ? "OK" : "KO";
+            Console.WriteLine($"Text is { ok }");
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Verifica se il testo passato contiene un numero coerente di parentesi
+        /// di apertura e chiusura "(", "[", "{"
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        static bool CheckBrackets(string text)
+        {
+
+        }
+    }
+}
